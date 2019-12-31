4ab1650a8cd05bb81881fc6d16c5f0349ffb85de
Benjamin Xia <bzijiexia@gmail.com>
2019-12-30 23:31:42 -0600

    Added random integer button

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
+import random
+
 app = Flask(__name__)
 
-@app.route('/')
+CORS(app)
+
+@app.route('/hello_world')
+@cross_origin()
 def hello_world():
     print('Called Hello World')
-    return 'Hello, World!'
+    return { "response": 'Hello, World!',
+             "random": "{}".format(random.randint(0, 10)) }, 200
--- src/App.js
+++ src/App.js
@@ -2,25 +2,66 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BACKEND_URL = "http://localhost:5000"
+
+class App extends React.Component {
+
+  state = { "message": "test",
+            "randint": "0"
+          }
+
+  getMessage() {
+    return fetch(`${BACKEND_URL}/hello_world`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      })
+      .then(response => {
+        console.log(response)
+        if (response.status === 200) {
+          return response.json()
+        }
+      })
+  }
+
+  componentDidMount() {
+    console.log("componentWillMount")
+    this.getMessage().then(res => {
+      console.log(res)
+      this.setState({ message: res["response"] })
+    })
+  }
+
+  randomNumber() {
+    this.getMessage().then(res => {
+      console.log(res)
+      this.setState({ randint: res["random"] })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload. message is { this.state.message }
+          </p>
+          <p>
+            Random integer: { this.state.randint }
+          </p>
+          <button
+            type="button"
+            onClick={this.randomNumber.bind(this)}
+          >
+            Random integer
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
